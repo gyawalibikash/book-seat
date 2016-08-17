9d1c8c23e15d74917459172091e0106b5055bcaf
Bikash Gyawali <gyawalibikash007@gmail.com>
2016-08-17 11:11:45 +0545

    redirected to profile create

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/profile';
+    protected $redirectTo = '/profile/create';
 
     /**
      * Create a new authentication controller instance.
