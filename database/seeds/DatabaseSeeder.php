ee2238db73df37234b5cb6d5e5344c4772c99236
Bikash Gyawali <gyawalibikash007@gmail.com>
2016-08-24 11:40:09 +0545

    created roles table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Eloquent\Model;
 
 use App\Profile;
 use App\User;
+use App\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -26,6 +27,7 @@ class UserAppSeed extends Seeder{
     public function run(){
         DB::table('users')->delete();
         DB::table('profiles')->delete();
+        DB::table('roles')->delete();
 
         $ram=User::create(array('name'=>'ram','email'=>'ram@gmail.com','password'=>bcrypt('password')));
         $shyam=User::create(array('name'=>'shyam','email'=>'shyam@gmail.com','password'=>bcrypt('password')));
@@ -36,6 +38,10 @@ class UserAppSeed extends Seeder{
         Profile::create(array('address'=>'jawlakhel, Lalitpur','contact_no'=>'01456345','gender'=>'male','user_id'=>$shyam->id));
         Profile::create(array('address'=>'Kalanki, Kathmandu','contact_no'=>'015675632','gender'=>'female','user_id'=>$geeta->id));
         $this->command->info('Profile Seeded Successfully');
+
+        Role::create(array('name'=>'ROLE_ADMIN'));
+        Role::create(array('name'=>'ROLE_USER'));
+        $this->command->info('Role Seeded Successfully');
 
 
     }
